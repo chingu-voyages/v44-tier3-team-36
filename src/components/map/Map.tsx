df074b79055cecf9cbfe9b08cc87dd5403e04f10
Beyhan <btstructure97@gmail.com>
2023-06-25 19:17:02 -0400

    attempting to get markers on map

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -1,9 +1,8 @@
-import { MapContainer, TileLayer, Marker} from "react-leaflet";
-import stopsData from "../data/stations.json"
+import { MapContainer, TileLayer} from "react-leaflet";
+// import stopsData from "../data/stations.json"
 
  
 const position: [number, number] = [40.7128, -74.006];
-const stopCoordinates: [number, number] = [latitude, longitude]
 
 function Map() {
   return (
@@ -18,7 +17,6 @@ function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright%22%3EOpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker stopCoordinates={stopCoordinates} />
       </MapContainer>
     </div>
   );
