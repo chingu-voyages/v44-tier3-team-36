29063f125dae4e5a8f205c8724d9a2d70bfc2368
Beyhan <btstructure97@gmail.com>
2023-08-03 12:41:44 -0400

    2 separate markers

--- src/components/map/GetIconForTrain.tsx
+++ src/components/map/GetIconForTrain.tsx
@@ -1,25 +1,25 @@
 import { Marker } from "react-leaflet";
 import { IconData } from "./AllIcons";
 import L from "leaflet";
-import arrow from "./arrow.png";
-import "./GetIconForTrain.css"
+import arrowImage from "./arrow.png";
+import "./GetIconForTrain.css";
 
 function GetIconForTrain({ trainLocations, selectedLine }) {
-//   const combinedMarker = () => {};
+  const arrowIcon = (offset) => {
+    const arrow = arrowImage;
+    const arrowIcon = L.icon({
+      iconUrl: arrow,
+      iconSize: [30, 30],
+    });
+
+    return { icon: arrowIcon, offset: offset };
+  };
 
   const getIconForTrain = () => {
     const iconData = IconData[selectedLine] || null;
-
-    const iconHtml = `
-    <div class="train-icon">
-        <img src="${arrow}" class="arrow-icon">
-        <img src="${iconData.data}" class="train-icon-img">
-    </div>
-  `;
-
-    const trainIcon = L.divIcon({
-      html: iconHtml,
-      iconSize: [20, 10],
+    const trainIcon = L.icon({
+      iconUrl: iconData.data,
+      iconSize: [30, 30],
     });
 
     return trainIcon;
@@ -35,21 +35,31 @@ function GetIconForTrain({ trainLocations, selectedLine }) {
           return (
             <div key={`train_${trainLetter}`}>
               {northBoundTrains.map((northTrain, index) => (
-                <Marker
-                  key={`north_${trainLetter}_${index}`}
-                  position={[northTrain.latitude, northTrain.longitude]}
-                  icon={getIconForTrain()}
-                >
-                </Marker>
+                <div key={`north_${trainLetter}_${index}`}>
+                  <Marker
+                    position={[northTrain.latitude - arrowIcon(-0.0002).offset, northTrain.longitude]}
+                    icon={arrowIcon(-0.0002).icon}
+                  />
+                  <Marker
+                    key={`north_${trainLetter}_${index}`}
+                    position={[northTrain.latitude, northTrain.longitude]}
+                    icon={getIconForTrain()}
+                  />
+                </div>
               ))}
 
               {southBoundTrains.map((southTrain, index) => (
-                <Marker
-                  key={`south_${trainLetter}_${index}`}
-                  position={[southTrain.latitude, southTrain.longitude]}
-                  icon={getIconForTrain()}
-                >
-                </Marker>
+                <div key={`south_${trainLetter}_${index}`}>
+                  <Marker
+                    position={[southTrain.latitude - arrowIcon(-0.0002).offset, southTrain.longitude]}
+                    icon={arrowIcon(-0.0002).icon}
+                  />
+                  <Marker
+                    key={`south_${trainLetter}_${index}`}
+                    position={[southTrain.latitude, southTrain.longitude]}
+                    icon={getIconForTrain()}
+                  />
+                </div>
               ))}
             </div>
           );
