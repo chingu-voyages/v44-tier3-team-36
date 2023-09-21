eca83eb4b21d784776fa7aca407ea8ae85923409
Beyhan <btstructure97@gmail.com>
2023-09-21 17:55:30 -0400

    several ways top get arrow bearing

--- src/components/map/GetIconForTrain.tsx
+++ src/components/map/GetIconForTrain.tsx
@@ -63,7 +63,7 @@ function GetIconForTrain({ trainLocations, selectedLine }) {
         if (trainLetter === selectedLine) {
           return (
             <div key={`train_${trainLetter}`}>
-              {/* {northBoundTrains.map((northTrain, index) => (
+              {northBoundTrains.map((northTrain, index) => (
                 <div key={`north_${trainLetter}_${index}`}>
                   <Marker
                     key={`north_${trainLetter}_${index}`}
@@ -75,7 +75,7 @@ function GetIconForTrain({ trainLocations, selectedLine }) {
                     </Tooltip>
                   </Marker>
                 </div>
-              ))} */}
+              ))}
               {southBoundTrains.map((southTrain, index) => (
                 <div key={`south_${trainLetter}_${index}`}>
                   <Marker
@@ -97,3 +97,188 @@ function GetIconForTrain({ trainLocations, selectedLine }) {
 }
 
 export default GetIconForTrain;
+
+// !!!! its really hard to see the arrows you have to zoom in, its hard to see because its inside the circle, the styling is done in the function, not the css, it also uses the arrow png
+
+// import { Marker } from "react-leaflet";
+// import { IconData } from "./AllIcons";
+// import L from "leaflet";
+// import arrowImage from "./arrow.png";
+// import "./GetIconForTrain.css";
+
+// function GetIconForTrain({ trainLocations, selectedLine }) {
+//   const arrowIcon = (bearing) => {
+//     const arrow = arrowImage;
+//     const arrowIcon = L.divIcon({
+//       html: `<img src="${arrow}" style="transform: rotate(${
+//         (bearing * 180) / Math.PI
+//       }deg);  height: 20px; width: 15px" />`,
+//       iconSize: [30, 30],
+//       iconAnchor: [15, 15],
+//       popupAnchor: [0, -15],
+//       className: "arrow-icon",
+//     });
+
+//     return arrowIcon;
+//   };
+
+//   const southArrowIcon = (bearing) => {
+//     const arrow = arrowImage;
+//     const arrowIcon = L.divIcon({
+//       html: `<img src="${arrow}" style="transform: rotate(${
+//         (bearing * 180) / Math.PI
+//       }deg);  height: 20px; width: 15px" />`,
+//       iconSize: [30, 30],
+//       iconAnchor: [15, 15],
+//       popupAnchor: [0, -15],
+//       className: "arrow-icon",
+//     });
+
+//     return arrowIcon;
+//   };
+
+//   const getIconForTrain = () => {
+//     const iconData = IconData[selectedLine] || null;
+//     const trainIcon = L.icon({
+//       iconUrl: iconData.data,
+//       iconSize: [30, 30],
+//       className: "train-icon",
+//     });
+
+//     return trainIcon;
+//   };
+
+//   return (
+//     <div>
+//       {Object.entries(trainLocations).map(([trainLetter, trainData]) => {
+//         const northBoundTrains = trainData.north || [];
+//         const southBoundTrains = trainData.south || [];
+
+//         if (trainLetter === selectedLine) {
+//           return (
+//             <div key={`train_${trainLetter}`}>
+//               {northBoundTrains.map((northTrain, index) => (
+//                 <div key={`north_${trainLetter}_${index}`}>
+//                   <Marker
+//                     position={[northTrain.latitude, northTrain.longitude]}
+//                     icon={arrowIcon(northTrain.bearing)}
+//                     zIndexOffset={1000}
+//                   />
+//                   <Marker
+//                     key={`north_${trainLetter}_${index}`}
+//                     position={[northTrain.latitude, northTrain.longitude]}
+//                     icon={getIconForTrain()}
+//                   />
+//                 </div>
+//               ))}
+
+//               {southBoundTrains.map((southTrain, index) => (
+//                 <div key={`south_${trainLetter}_${index}`}>
+//                   <Marker
+//                     position={[southTrain.latitude, southTrain.longitude]}
+//                     icon={southArrowIcon(southTrain.bearing)}
+//                     zIndexOffset={1000}
+//                   />
+//                   <Marker
+//                     key={`south_${trainLetter}_${index}`}
+//                     position={[southTrain.latitude, southTrain.longitude]}
+//                     icon={getIconForTrain()}
+//                   />
+//                 </div>
+//               ))}
+//             </div>
+//           );
+//         }
+//         return null;
+//       })}
+//     </div>
+//   );
+// }
+
+// export default GetIconForTrain;
+
+
+
+//!! using the arrow png here as well, this was where we had difficulties b/c a white box would appear via html. the offset was recommended by chatgpt so the arrow wouldn't be on top of the train icon
+// import { Marker } from "react-leaflet";
+// import { IconData } from "./AllIcons";
+// import L from "leaflet";
+// import arrowImage from "./arrow.png";
+// import "./GetIconForTrain.css";
+
+// function GetIconForTrain({ trainLocations, selectedLine }) {
+//   const arrowIcon = (offset, bearing) => {
+//     const arrow = arrowImage;
+//     const arrowIcon = L.divIcon({
+//       iconUrl: arrow,
+//       html: `<img src="${arrow}" style="transform: rotate(${
+//         (bearing * 180) / Math.PI
+//       }deg);  height: 20px; width: 15px" />`,
+//       iconSize: [30, 30],
+//     });
+
+//     return { icon: arrowIcon, offset: offset };
+//   };
+
+//   const getIconForTrain = () => {
+//     const iconData = IconData[selectedLine] || null;
+//     const trainIcon = L.icon({
+//       iconUrl: iconData.data,
+//       iconSize: [30, 30],
+//     });
+
+//     return trainIcon;
+//   };
+
+//   return (
+//     <div>
+//       {Object.entries(trainLocations).map(([trainLetter, trainData]) => {
+//         const northBoundTrains = trainData.north || [];
+//         const southBoundTrains = trainData.south || [];
+
+//         if (trainLetter === selectedLine) {
+//           return (
+//             <div key={`train_${trainLetter}`}>
+//               {northBoundTrains.map((northTrain, index) => (
+//                 <div key={`north_${trainLetter}_${index}`}>
+//                   <Marker
+//                     position={[
+//                       northTrain.latitude - arrowIcon(-0.0002).offset,
+//                       northTrain.longitude,
+//                     ]}
+//                     icon={arrowIcon(-0.0002).icon}
+//                   />
+//                   <Marker
+//                     key={`north_${trainLetter}_${index}`}
+//                     position={[northTrain.latitude, northTrain.longitude]}
+//                     icon={getIconForTrain()}
+//                   />
+//                 </div>
+//               ))}
+
+//               {southBoundTrains.map((southTrain, index) => (
+//                 <div key={`south_${trainLetter}_${index}`}>
+//                   <Marker
+//                     position={[
+//                       southTrain.latitude - arrowIcon(-0.0002).offset,
+//                       southTrain.longitude,
+//                     ]}
+//                     icon={arrowIcon(-0.0002).icon}
+//                   />
+//                   <Marker
+//                     key={`south_${trainLetter}_${index}`}
+//                     position={[southTrain.latitude, southTrain.longitude]}
+//                     icon={getIconForTrain()}
+//                   />
+//                 </div>
+//               ))}
+//             </div>
+//           );
+//         }
+//         return null;
+//       })}
+//     </div>
+//   );
+// }
+
+// export default GetIconForTrain;
